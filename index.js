f4f53a541fb5fdff92abbf94eb87384b410db131
nazlev <64920704+nazlev@users.noreply.github.com>
2023-01-14 01:37:42 +0300

    first node

---
+++ index.js
@@ -0,0 +1,47 @@
+const colors = require('colors')
+
+const [from, to] = process.argv.slice(2).map(item => parseInt(item))
+const {green, yellow, red} = colors;
+const collection = [green, yellow, red]
+let idx = 0
+
+const colorPrint = (num) => {
+    console.log(collection[idx](num))
+    if(idx === collection.length - 1) {
+        idx = 0
+    } else {
+        idx++
+    }
+}
+
+if (isNaN(from) || isNaN(to)) { 
+    console.error(colors.red('Incorrected numbers'))
+    process.exit(0)
+}
+
+
+const isPrime = (num) => {
+    if(num < 2) {
+        return false
+    }
+    let count = 2;
+    while(count < num) {
+        if (num % count === 0) return false
+        count++
+    }
+    return true
+}
+ 
+let flag = false
+let count = from
+while(count <= to) {
+    if(isPrime(count)) {
+        colorPrint(count)
+        flag = true
+    }
+    count++
+}
+
+if(!flag) {
+    console.log(red('No digital diapazone'))
+}
